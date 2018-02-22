95ccd6cfdd36457e63c413760462f108f1ae76fd
enhering <enhering@gmail.com>
2018-02-22 15:30:53 -0400

    Changing matirx processing

--- src/speckle-analyzer.cpp
+++ src/speckle-analyzer.cpp
@@ -10,15 +10,15 @@ int main(int, char**)
 
     Mat edges;
     namedWindow("edges",1);
-    for(;;)
-    {
-        Mat frame;
-        cap >> frame; // get a new frame from camera
-        cvtColor(frame, edges, CV_BGR2GRAY);
-        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
-        Canny(edges, edges, 0, 30, 3);
-        imshow("edges", edges);
-        if(waitKey(30) >= 0) break;
+
+    for(;;) {
+      Mat frame;
+      cap >> frame; // get a new frame from camera
+      cvtColor(frame, edges, CV_BGR2GRAY);
+      //GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
+      // Canny(edges, edges, 0, 30, 3);
+      imshow("edges", edges);
+      if(waitKey(30) >= 0) break;
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
