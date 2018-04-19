92308f039d24a893119cc4b154eb2f9a28c92ebe
ENHering(lab) <ENHering(lab)@localhost>
2018-04-19 11:12:23 -0400

    grabbing 5 frames

--- src/classes/DC1394Wrapper.cpp
+++ src/classes/DC1394Wrapper.cpp
@@ -71,7 +71,7 @@ int DC1394Wrapper::Init() {
   // std::cout << "Done." << std::endl;
 
   std::cout << "Setting ISO speed... ";
-  m_eErr=dc1394_video_set_iso_speed(m_pcCamera, DC1394_ISO_SPEED_100);
+  m_eErr=dc1394_video_set_iso_speed(m_pcCamera, DC1394_ISO_SPEED_400);
   CheckError(2);
   std::cout << "Done." << std::endl;
 
@@ -89,9 +89,9 @@ int DC1394Wrapper::Init() {
   // m_eErr = dc1394_external_trigger_set_mode(m_pcCamera, DC1394_TRIGGER_MODE_3);
   m_eErr = dc1394_external_trigger_set_power(m_pcCamera, DC1394_ON);
   // CheckError(22);
-  // m_eErr = dc1394_external_trigger_set_source(m_pcCamera, DC1394_TRIGGER_SOURCE_SOFTWARE);
+  m_eErr = dc1394_external_trigger_set_source(m_pcCamera, DC1394_TRIGGER_SOURCE_SOFTWARE);
   // m_eErr = dc1394_feature_set_value(m_pcCamera, DC1394_FEATURE_TRIGGER, DC1394_FALSE);
-  CheckError(23);
+  // CheckError(23);
   std::cout << "Done." << std::endl;
 
   std::cout << "Setting capture flags... ";
@@ -108,10 +108,10 @@ void DC1394Wrapper::Grab() {
     StartTransmission();
   }
 
-  // std::cout << "Pullling trigger... ";
-  // m_eErr = dc1394_software_trigger_set_power(m_pcCamera, DC1394_ON);
-  // CheckError(24);
-  // std::cout << "Ok" << std::endl;
+  std::cout << "Pullling trigger... ";
+  m_eErr = dc1394_software_trigger_set_power(m_pcCamera, DC1394_ON);
+  CheckError(24);
+  std::cout << "Ok" << std::endl;
 
   std::cout << "Capturing... " << std::endl;
   while (! bCaptureOK) {
