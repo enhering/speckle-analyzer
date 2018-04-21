2ceb2565931ceb5d15a2acdf3128903d2f3ca2b5
enhering <enhering@gmail.com>
2018-04-21 10:57:50 -0400

    Working at home

--- src/classes/DC1394Wrapper.cpp
+++ src/classes/DC1394Wrapper.cpp
@@ -153,7 +153,7 @@ void DC1394Wrapper::Grab() {
   CheckError(24);
   std::cout << "Ok" << std::endl;
 
-  std::cout << "Capturing... ";
+  std::cout << "Capturing... " << std::endl;
   while (! bCaptureOK) {
     m_eErr=dc1394_capture_dequeue(m_pcCamera, DC1394_CAPTURE_POLICY_WAIT, &m_pcFrame);
     CheckError(6);
